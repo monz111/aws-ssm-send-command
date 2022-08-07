d456f3dcce79ca7cc72af9b5d556c05c57b36f55
yuya.mozen <yuya.monzen@edge-inc.co.jp>
2022-08-07 17:39:26 +0900

    📦️ build

--- dist/index.js
+++ dist/index.js
@@ -18,6 +18,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -43,13 +52,30 @@ try {
             workingDirectory: [inputs.workingDirectory],
             commands: [inputs.command],
         },
-    }, (err, data) => {
+    }, (err, data) => __awaiter(void 0, void 0, void 0, function* () {
         var _a;
-        if (err)
+        if (err || !((_a = data.Command) === null || _a === void 0 ? void 0 : _a.CommandId))
             throw err;
+        const CommandId = data.Command.CommandId;
+        const checks = new Array(inputs.checkStatusLimit).fill(1);
+        for (const InstanceId of inputs.instanceIds) {
+            for (const _ of checks) {
+                yield new Promise(resolve => setTimeout(resolve, inputs.checkStatusFrequency * 1000));
+                const invocation = yield ssm.getCommandInvocation({ CommandId, InstanceId }).promise();
+                if (invocation.Status === 'Failed') {
+                    core.setFailed('Faild');
+                    break;
+                }
+                else if (invocation.Status === 'Success') {
+                    core.setOutput("output-contents", invocation);
+                    break;
+                }
+                console.log(invocation);
+            }
+        }
         console.log(data);
-        core.setOutput("command-id", (_a = data.Command) === null || _a === void 0 ? void 0 : _a.CommandId);
-    });
+        core.setOutput("command-id", CommandId);
+    }));
 }
 catch (err) {
     console.error(err, err.stack);
@@ -67,10 +93,10 @@ function SanitizeInputs() {
     const _command = core.getInput("command");
     const _workingDirectory = core.getInput("working-directory");
     const _comment = core.getInput("comment");
+    const _checkStatusLimit = core.getInput("check-status-limit");
+    const _checkStatusFrequency = core.getInput("check-status-frequency");
     // customized not supported yet, will be updated soon.
     const _documentName = "AWS-RunShellScript";
-    const _outputS3BucketName = "your-s3-bucket-name";
-    const _outputS3KeyPrefix = "your-s3-bucket-directory-name";
     return {
         accessKeyId: _accessKeyId,
         secretAccessKey: _secretAccessKey,
@@ -80,5 +106,7 @@ function SanitizeInputs() {
         documentName: _documentName,
         workingDirectory: _workingDirectory,
         comment: _comment,
+        checkStatusLimit: +_checkStatusLimit || 0,
+        checkStatusFrequency: +_checkStatusFrequency || 1
     };
 }
